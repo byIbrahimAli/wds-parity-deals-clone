5c1a5fc8fcc33a8e28c12f6cd352af2cbd11cab7
byIbrahimAli <59145840+byIbrahimAli@users.noreply.github.com>
2024-10-31 10:28:36 +0000

    Adding links to navbar including dynamic links depending on if user is signed in - using clerk for auth.
    Also downgrading react cos issues with peer dependencies.

--- src/app/(marketing)/_components/NavBar.tsx
+++ src/app/(marketing)/_components/NavBar.tsx
@@ -1,13 +1,32 @@
 import { BrandLogo } from '@/components/BrandLogo'
+import { SignedIn, SignedOut, SignInButton } from '@clerk/nextjs'
 import Link from 'next/link'
 
 export function NavBar() {
   return (
-    <header>
+    <header className='flex py-6 shadow-xl top-0 w-full z-10 bg-background/95'>
       <nav className='flex items-center gap-10 container font-semibold'>
         <Link href='/' className='mr-auto'>
           <BrandLogo />
         </Link>
+        <Link className='text-lg' href='#'>
+          Features
+        </Link>
+        <Link className='text-lg' href='/#pricing'>
+          Pricing
+        </Link>
+        <Link className='text-lg' href='#'>
+          About
+        </Link>
+        <span className='text-lg'>
+          <SignedIn>
+            {/* only renders content if user is signed in */}
+            <Link href='/dashboard'>Dashboard</Link>
+          </SignedIn>
+          <SignedOut>
+            <SignInButton>Login</SignInButton>
+          </SignedOut>
+        </span>
       </nav>
     </header>
   )
